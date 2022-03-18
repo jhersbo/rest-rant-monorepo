af1e9b4971f82b4cfe063a2ca756723e81098ee6
jhersbo <jhersbo@gmail.com>
2022-03-17 19:12:20 -0500

    done

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,8 +1,27 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
+
+router.get('/profile', async(req, res)=>{
+    try{
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+        if(authenticationMethod == 'Bearer'){
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+            const { id } = result.value
+            let user = await User.findOne({
+                where:{
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
+    }catch{
+        res.json(null)
+    }
+})
 
 router.post('/', async (req, res)=>{
     let user = await User.findOne({
@@ -15,7 +34,8 @@ router.post('/', async (req, res)=>{
             message: 'No work'
         })
     }else{
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, {id: user.userId})
+        res.json({ user: user, token: result.value })
     }
 })
 
